be80108fbc9315bab85067868769a87de9b8e677
Edon Abdullahu <edonabdullahu2@gmail.com>
2023-06-29 14:45:42 -0400

    Merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import { Header } from './componets/Header';
 import { Routes, Route } from 'react-router-dom';
@@ -19,28 +18,6 @@ function App() {
           <Route path='/about' element={<About />} />
         </Routes>
       </div>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> Live
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,14 +10,6 @@ root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-=======
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> Live
 );
 
 // If you want to start measuring performance in your app, pass a function
